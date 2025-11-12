59b472839b03153a8f1038625d3844192dcc0520
Charlie-robin <charlie.robin.richardson@gmail.com>
2025-11-12 17:14:53 +0000

    Removed starter Ex4

--- src/exercises/Exercise4/Exercise4.jsx
+++ src/exercises/Exercise4/Exercise4.jsx
@@ -3,22 +3,20 @@ import "./Exercise4.css";
 import AlbumTiles from "../../components/AlbumTiles/AlbumTiles";
 
 const Exercise4 = ({ albums }) => {
-  const filteredAlbums = albums.filter((album) => album.strAlbumThumb);
 
-  const highestRating = filteredAlbums.sort((a, b) => b.intScore - a.intScore);
 
   return (
     <section className="discography">
       <h2 className="discography__heading">E4 : Discography</h2>
 
       <div className="all-albums">
-        <AlbumTiles title="Albums" albumsArr={filteredAlbums.slice(0, 9)} />
+        <AlbumTiles title="Albums" albumsArr={albums} />
       </div>
 
       <div className="highest-rated">
         <AlbumTiles
           title="Highest Rated"
-          albumsArr={highestRating.slice(0, 9)}
+          albumsArr={albums}
         />
       </div>
     </section>
